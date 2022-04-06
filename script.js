50f7bdc715704437a52aa9611bb95d1463b55a6f
Lean <93644347+092PopSmoke@users.noreply.github.com>
2022-04-06 10:03:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function Colorchanger0() {
+    document.body.style.backgroundColor = "Purple";
+    document.getElementById("yes").style.color = "white";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
+
+function Colorchanger1() {
+    document.body.style.backgroundColor = "Blue";
+    document.getElementById("yes").style.color = "white";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
+
+function Colorchanger2() {
+    document.body.style.backgroundColor = "Red";
+    document.getElementById("yes").style.color = "white";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
+
+function Colorchanger3() {
+    document.body.style.backgroundColor = "Green";
+    document.getElementById("yes").style.color = "white";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
+
+function Colorchanger4() {
+    document.body.style.backgroundColor = "Yellow";
+    document.getElementById("yes").style.color = "white";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
+
+function Colorchanger5() {
+    document.body.style.backgroundColor = "White";
+    document.getElementById("yes").style.color = "black";
+    document.getElementById("yes").style.fontWeight = "bolder";
+}
